afdc21309cf06bf30ece7c548b6b374872078c8e
HamzaIjaz1 <iamhamzza@gmail.com>
2019-04-10 20:59:13 +0500

    added categories

---
+++ resources/js/Components/Categories.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Card } from 'antd';
+class Categories extends Component {
+    state = {}
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Categories;
